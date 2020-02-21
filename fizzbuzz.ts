060862376604a23180fcbf446bdfdf22d5c67062
Andrew Huth <andrew.huth@airbnb.com>
2020-02-20 20:53:41 -0800

    Add FizzBuzzNth

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -19,6 +19,12 @@ type _7 = Increment<_6>;
 type _8 = Increment<_7>;
 type _9 = Increment<_8>;
 type _10 = Increment<_9>;
+type _11 = Increment<_10>;
+type _12 = Increment<_11>;
+type _13 = Increment<_12>;
+type _14 = Increment<_13>;
+type _15 = Increment<_14>;
+type _16 = Increment<_15>;
 
 type Decrement<N> = N extends Increment<infer D>
   ? D
@@ -75,4 +81,28 @@ type Test_And = [
   Eq<And<true, false>, false>,
   Eq<And<false, true>, false>,
   Eq<And<false, false>, false>,
+];
+
+type IsDivisibleBy3<N> = IsDivisibleBy<N, _3>;
+type IsDivisibleBy5<N> = IsDivisibleBy<N, _5>;
+type IsDivisibleBy15<N> = And<IsDivisibleBy3<N>, IsDivisibleBy5<N>>;
+
+type FizzBuzzNth<N> = IsDivisibleBy15<N> extends true
+  ? 'FizzBuzz'
+  : IsDivisibleBy3<N> extends true
+    ? 'Fizz'
+    : IsDivisibleBy5<N> extends true
+      ? 'Buzz'
+      : N;
+
+type Test_FizzBuzzNth = [
+  Eq<FizzBuzzNth<_1>, _1>,
+  Eq<FizzBuzzNth<_2>, _2>,
+  Eq<FizzBuzzNth<_3>, "Fizz">,
+  Eq<FizzBuzzNth<_4>, _4>,
+  Eq<FizzBuzzNth<_5>, "Buzz">,
+  Eq<FizzBuzzNth<_6>, "Fizz">,
+  Eq<FizzBuzzNth<_14>, _14>,
+  Eq<FizzBuzzNth<_15>, "FizzBuzz">,
+  Eq<FizzBuzzNth<_16>, _16>
 ];
