64e9f1f83f653a851acf18c1d6e0947be4b5d45e
Andrew Huth <andrew.huth@airbnb.com>
2020-02-20 20:22:14 -0800

    Add subtraction

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,9 +1,9 @@
 type Eq<A, B extends A> = 'passes';
 
-type TestEqual = [
+type Test_Equal = [
   Eq<'Hello', 'Hello'>, // Passes
   Eq<'Hello', 'World'>, // Fails
-]
+];
 
 type NAN = 'invalid number';
 type _0 = 0;
@@ -19,3 +19,28 @@ type _7 = Increment<_6>;
 type _8 = Increment<_7>;
 type _9 = Increment<_8>;
 type _10 = Increment<_9>;
+
+type Decrement<N> = N extends Increment<infer D>
+  ? D
+  : NAN;
+
+type Test_Decrement = [
+  Eq<Decrement<_0>, NAN>, // Passes
+  Eq<Decrement<_1>, _0>, // Passes
+  Eq<Decrement<_2>, _1>, // Passes
+  Eq<Decrement<Decrement<_5>>, _3>, // Passes
+  Eq<Decrement<Decrement<_5>>, _2>, // Fails
+];
+
+type Subtract<N, Amount> = {
+  amount_is_zero: N;
+  recurse: Subtract<Decrement<N>, Decrement<Amount>>;
+}[Amount extends _0 ? 'amount_is_zero' : 'recurse'];
+
+type Test_Subtract = [
+  Eq<Subtract<_1, _0>, _1>, // Passes
+  Eq<Subtract<_1, _1>, _0>, // Passes
+  Eq<Subtract<_9, _4>, _5>, // Passes
+  Eq<Subtract<_1, _2>, NAN>, // Passes
+  Eq<Subtract<_9, _2>, _3>, // Fails
+];
